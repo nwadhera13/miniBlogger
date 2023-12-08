bb31970c46dafc0bb1b1b19def61926195ad85e4
nwadhera13 <nwadhera13@gmail.com>
2023-12-08 19:39:25 +0530

    first commit

---
+++ app/page.jsx
@@ -0,0 +1,18 @@
+import Feed from "@components/Feed";
+
+const Home = () => (
+  <section className='w-full flex-center flex-col'>
+    <h1 className='head_text text-center'>
+      Discover what others have to say
+      <br className='max-md:hidden' />
+      <span className='orange_gradient text-center'> And share your own thoughts</span>
+    </h1>
+    <p className='desc text-center'>
+      miniBlogger is a fast-paced idea sharing application where you can share your thoughts in short form and search for your favourite topics/users and discover what they have to say.
+    </p>
+
+    <Feed />
+  </section>
+);
+
+export default Home;
