c68c44817abb341d3334909c4b60fbc4cfd26e49
nwadhera13 <68321799+nwadhera13@users.noreply.github.com>
2023-12-08 20:18:58 +0530

    Update Form.jsx

--- components/Form.jsx
+++ components/Form.jsx
@@ -7,8 +7,7 @@ const Form = ({ type, post, setPost, submitting, handleSubmit }) => {
         <span className='blue_gradient'>{type} Post</span>
       </h1>
       <p className='desc text-left max-w-md'>
-        {type} and share amazing prompts with the world, and let your
-        imagination run wild with any AI-powered platform
+        {type} and share amazing ideas with the world.
       </p>
 
       <form
